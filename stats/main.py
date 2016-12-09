c13feb7aa92a79dff55421b1477e749bcef96902
OpsMx <accounts@opsmx.com>
2016-12-09 13:35:57 +0530

    Create main.py

---
+++ stats/main.py
@@ -0,0 +1,13 @@
+import cpu_util
+import docker_stats
+import time
+import traceback
+
+while True:
+    try:
+        cpu_util.main()
+        docker_stats.main()
+        time.sleep(3)
+    except:
+        print "=========TRACEBACK-MAIN========="
+        print traceback.format_exc()
